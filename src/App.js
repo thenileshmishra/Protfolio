25617c5beaf955bb6f7c4349b04103688f7a6700
thenileshmishra <nileshmishra175@gmail.com>
2023-05-29 23:18:21 +0530

    Header Section Partially Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
 import Header from "./components/header/Header";
-import Home from "./components/home/Home";
 function App() {
   return (
     <>
       <Header />
-      <main className="main">
-        <Home />;
-      </main>
     </>
   );
 }
