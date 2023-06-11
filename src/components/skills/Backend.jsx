08bc474b95b55f41131232d382ae9b2e05a4747d
thenileshmishra <nileshmishra175@gmail.com>
2023-06-11 23:47:10 +0530

    frontend

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Backend() {
+  return <div>Backend</div>;
+}
+
+export default Backend;
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./skills.css";
+import Frontend from "./Frontend";
+import Backend from "./Backend";
+
+function Skills() {
+  return (
+    <section className="skills section" id="skills">
+      <h2 className="section__title">Skills</h2>
+      <span className="section__subtitle">My Technical Skills</span>
+      <div className="skills__container container grid"></div>
+    </section>
+  );
+}
+
+export default Skills;
