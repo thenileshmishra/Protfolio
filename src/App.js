a0a7f535e3883c9ab8b826b2451204ea55cc8e50
thenileshmishra <nileshmishra175@gmail.com>
2023-06-13 22:57:56 +0530

    qualification

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
+import Qualification from "./components/qualification/Qualification";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         {/* <Home /> */}
         {/* <About /> */}
         {/* <Skills /> */}
-        <Services />
+        {/* <Services /> */}
+        <Qualification />
       </main>
     </>
   );
