b2d8984c2fd4cd87e4c360bf54ca0dac6e0ab001
Nilesh Mishra <thenileshmishra@gmail.com>
2025-05-03 17:40:16 +0530

    Skills section is updated

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -8,6 +8,7 @@ function Skills() {
       <span className="section__subtitle">My Technical Skills</span>
       <div className="skills__container container grid">
         <div className="skills__content">
+          {/* Machine Learning / AI Skills */}
           <div className="skills__section">
             <h3 className="skills__title">Machine Learning</h3>
             <div className="skills__paragraph">
@@ -19,64 +20,51 @@ function Skills() {
                 </div>
               </div>
               <div className="skills__data">
-                <i className="bx bx-network-chart"></i>
-                <div>
-                  <h3 className="skills__name">Neural Networks</h3>
-                  <span className="skills__level">Advanced</span>
-                </div>
-              </div>
-              <div className="skills__data">
-                <i className="bx bx-chart"></i>
+                <i className="bx bx-message-square-detail"></i>
                 <div>
                   <h3 className="skills__name">NLP</h3>
-                  <span className="skills__level">Intermediate</span>
-                </div>
-              </div>
-              <div className="skills__data">
-                <i className="bx bx-data"></i>
-                <div>
-                  <h3 className="skills__name">TensorFlow</h3>
-                  <span className="skills__level">Intermediate</span>
-                </div>
-              </div>
-              <div className="skills__data">
-                <i className="bx bx-line-chart"></i>
-                <div>
-                  <h3 className="skills__name">PyTorch</h3>
-                  <span className="skills__level">Intermediate</span>
-                </div>
-              </div>
-              <div className="skills__data">
-                <i className="bx bx-code-alt"></i>
-                <div>
-                  <h3 className="skills__name">Python</h3>
                   <span className="skills__level">Advanced</span>
+                </div>
+              </div>
+              <div className="skills__data">
+                <i className="bx bx-network-chart"></i>
+                <div>
+                  <h3 className="skills__name">LLMs</h3>
+                  <span className="skills__level">Advanced</span>
+                </div>
+              </div>
+              <div className="skills__data">
+                <i className="bx bx-transfer"></i>
+                <div>
+                  <h3 className="skills__name">Transformers</h3>
+                  <span className="skills__level">Advanced</span>
+                </div>
+              </div>
+              <div className="skills__data">
+                <i className="bx bx-image"></i>
+                <div>
+                  <h3 className="skills__name">Computer Vision</h3>
+                  <span className="skills__level">Intermediate</span>
                 </div>
               </div>
             </div>
           </div>
 
+          {/* Full Stack Development */}
           <div className="skills__section">
-            <h3 className="skills__title">Fullstack Development</h3>
+            <h3 className="skills__title"> Full Stack Development</h3>
             <div className="skills__paragraph">
               <div className="skills__data">
                 <i className="bx bxl-react"></i>
                 <div>
-                  <h3 className="skills__name">React</h3>
+                  <h3 className="skills__name">React.js</h3>
                   <span className="skills__level">Advanced</span>
                 </div>
               </div>
               <div className="skills__data">
-                <i className="bx bxl-nodejs"></i>
+                <i className="bx bx-code-block"></i>
                 <div>
-                  <h3 className="skills__name">Node.js</h3>
-                  <span className="skills__level">Advanced</span>
-                </div>
-              </div>
-              <div className="skills__data">
-                <i className="bx bxl-mongodb"></i>
-                <div>
-                  <h3 className="skills__name">MongoDB</h3>
+                  <h3 className="skills__name">Node Js</h3>
                   <span className="skills__level">Intermediate</span>
                 </div>
               </div>
@@ -98,6 +86,77 @@ function Skills() {
                 <i className="bx bxl-css3"></i>
                 <div>
                   <h3 className="skills__name">CSS3</h3>
+                  <span className="skills__level">Advanced</span>
+                </div>
+              </div>
+              <div className="skills__data">
+                <i className="bx bxl-bootstrap"></i>
+                <div>
+                  <h3 className="skills__name">Bootstrap</h3>
+                  <span className="skills__level">Advanced</span>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+
+          {/* Tools & Frameworks */}
+          <div className="skills__section">
+            <h3 className="skills__title">Tools & Frameworks</h3>
+            <div className="skills__paragraph">
+              <div className="skills__data">
+                <i className="bx bxl-python"></i>
+                <div>
+                  <h3 className="skills__name">Python</h3>
+                  <span className="skills__level">Intermediate</span>
+                </div>
+              </div>
+              <div className="skills__data">
+                <i className="bx bx-code-curly"></i>
+                <div>
+                  <h3 className="skills__name">C++/C</h3>
+                  <span className="skills__level">Advanced</span>
+                </div>
+              </div>
+              <div className="skills__data">
+                <i className="bx bx-data"></i>
+                <div>
+                  <h3 className="skills__name">PyTorch</h3>
+                  <span className="skills__level">Advanced</span>
+                </div>
+              </div>
+              <div className="skills__data">
+                <i className="bx bx-line-chart"></i>
+                <div>
+                  <h3 className="skills__name">TensorFlow</h3>
+                  <span className="skills__level">Advanced</span>
+                </div>
+              </div>
+              <div className="skills__data">
+                <i className="bx bx-data"></i>
+                <div>
+                  <h3 className="skills__name">Scikit-Learn</h3>
+                  <span className="skills__level">Advanced</span>
+                </div>
+              </div>
+              <div className="skills__data">
+                <i className="bx bx-data"></i>
+                <div>
+                  <h3 className="skills__name">Pandas</h3>
+                  <span className="skills__level">Advanced</span>
+                </div>
+              </div>
+              <div className="skills__data">
+                <i className="bx bx-math"></i>
+                <div>
+                  <h3 className="skills__name">NumPy</h3>
+                  <span className="skills__level">Advanced</span>
+                </div>
+              </div>
+              <div className="skills__data">
+                <i className="bx bxl-github"></i>
+                <div>
+                  <h3 className="skills__name">Git & GitHub</h3>
                   <span className="skills__level">Advanced</span>
                 </div>
               </div>
